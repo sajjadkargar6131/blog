ef5b49aee9b9a29c293115bd1f071e4dc845c98d
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-11-24 11:24:34 +0330

    edit model and template

--- blog/models.py
+++ blog/models.py
@@ -26,3 +26,6 @@ class Comment(models.Model):
     text = models.TextField()
     post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name='comments')
     datetime_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return f'{self.user} : {self.text}'
