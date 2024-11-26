af2448d457743d49702b15e5ee738c7e836275ab
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-11-26 09:10:28 +0330

    update timezone

--- config/settings.py
+++ config/settings.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
