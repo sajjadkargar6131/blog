259b672d309d44149cf2eb20a077829b7257e78b
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-12-10 13:12:58 +0330

    custom filter for show the view's of post

---
+++ blog/templatetags/custom_filters.py
@@ -0,0 +1,16 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def format_number(value):
+    try:
+        if value >= 1000 and value < 1000000:
+            value = value / 1000
+            return f'{value:.1f}K'
+        if value >= 1000000 and value < 1000000000:
+            value = value / 1000000
+            return f'{value:.1f}M'
+        return value
+    except (ValueError, TypeError):
+        return value
