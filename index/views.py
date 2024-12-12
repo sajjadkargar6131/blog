77f279f5130aec00ce425e9e3e365c95648c7952
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-12-12 12:52:31 +0330

    archive system

--- index/views.py
+++ index/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.db.models import Count
+from blog.models import Post
 
 def index(request):
-    return render(request, 'index/index.html')
+    top_post = Post.objects.annotate(view_count=Count('views')).order_by('-view_count').first()
+    monthly_archive = Post.objects.dates('created_at', 'month', order='DESC')
+    return render(request, 'index/index.html', {'top_post':top_post, 'monthly_archive':monthly_archive})
