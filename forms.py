621c92acc469bc7a59f8804eec9f1dc1fc9f953e
sajjadkargar6131 <sajjad71kargar@gmail.com>
2024-12-18 13:39:47 +0330

    slove bug

---
+++ forms.py
@@ -0,0 +1,72 @@
+from django import forms
+from .models import Post, Category, Comment
+from taggit.forms import TagField
+
+class CustomTagfield(TagField):
+    def clean(self, value):
+        return [t.strip() for t in value.split() if t.strip()]
+   
+class PostCreateForm(forms.ModelForm):
+    tags = CustomTagfield()
+    tags = TagField(
+        help_text= "برچسب ها را با فاصله از هم جدا کنید.",
+        label = "تگ ها",
+        required=False,
+    )
+    new_category = forms.CharField(
+        max_length=100,
+        required=False,
+        label='ایجاد دسته بندی جدید'
+    )
+    class Meta:
+        model = Post
+        fields = ("title", "text", "status", "cover", "categories", "tags")
+        labels = {
+            'title' : 'عنوان',
+            'text' : 'متن',
+            'status' : 'وضعیت',
+            'cover' : 'عکس',  
+            'categories' : 'دسته بندی ها'
+        }
+        widgets={
+            'categories':forms.CheckboxSelectMultiple,
+        }
+        
+    def __init__(self, *args, **kwargs):
+        is_create = kwargs.pop('is_create', False)
+        super().__init__(*args, **kwargs)
+        if not is_create:
+            self.fields.pop('new_category')
+            
+    def save(self, commit=True):
+      
+         # ذخیره پست بدون تغییرات در categories
+        post_instance = super().save(commit=False)
+        
+        # ذخیره پست قبل از هر چیز
+        post_instance.save()
+
+        # افزودن دسته بندی جدید در صورتی که مشخص شده باشد
+        new_category_name = self.cleaned_data.get('new_category')
+        if new_category_name:
+            category, created = Category.objects.get_or_create(name=new_category_name)
+            post_instance.categories.add(category)  # افزودن دسته بندی جدید
+
+        # افزودن دسته بندی های انتخابی
+        categories = self.cleaned_data.get('categories')
+        if categories:
+            post_instance.categories.set(categories)  # تنظیم دسته بندی ها (حذف گزینه های قبلی و افزودن جدید)
+
+        tags = self.cleaned_data.get('tags')
+        if tags:
+            post_instance.tags.set(tags)  # استفاده از `set` برای تنظیم تگ‌ها
+        return post_instance
+                      
+class CommentForm(forms.ModelForm):
+    
+    class Meta :
+        model = Comment
+        fields = ('text', )
+        labels = {
+            'text' : 'نظر شما'
+        }
---
+++ urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, re_path
+from . import views
+urlpatterns = [
+    
+    path('create/', views.PostCreateView.as_view(), name='post_create'),
+    
+    path('', views.IndexListView.as_view(), name='blog_index'),
+    
+    # path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    re_path(r'^(?P<slug>[-\w\u0600-\u06FF]+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    
+    path('update/<str:slug>', views.PostUpdateView.as_view(), name='post_update'),
+    
+    path('delete/<str:slug>', views.PostDeleteView.as_view(), name='post_delete'),
+    
+    path('like/<int:post_id>/', views.like_post, name='like_post'),
+    
+    path('bookmark/<int:post_id>/', views.bookmark_post, name='bookmark_post'),
+    
+    path('archives/<int:year>/<int:month>/', views.archive_month, name='archive_month'),
+    
+    path('category/<str:name>/', views.PostListByCategoryView.as_view(), name='category_post'),
+    
+    re_path(r'^blog/tag/(?P<tag_slug>[-\w\u0600-\u06FF]+)/$', views.PostListByTagView.as_view(), name='posts_by_tag'),
+]
