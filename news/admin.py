28291072e40a546ca68577cabf6e3e20eb80832a
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-13 17:19:51 +0330

    fix bug for list_display

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import News
+from .models import News, NewsComment
 
 
 class AdminNews(admin.ModelAdmin):
     list_display = ['author', 'title', 'text', 'created_at', 'status']
 
 
+class AdminNewsComment(admin.ModelAdmin):
+    list_display = ['user', 'news', 'text', 'datetime_created', 'publish']
+
+
 admin.site.register(News, AdminNews)
+admin.site.register(NewsComment, AdminNewsComment)
