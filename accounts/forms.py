abf6b995e9cc2cd5617d23ab73136adf24196c9a
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-16 21:31:37 +0330

    Enable viewing and changing the profile picture

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,28 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
+from .models import CustomUser
 
-class CustomUserCreatinForm(UserCreationForm):
+
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = UserCreationForm.Meta.fields
-        
+
+
 class CustomUserChangeForm(UserChangeForm):
-    class Meta :
+    class Meta:
         model = get_user_model()
         fields = UserChangeForm.Meta.fields
-        
+
+
+class ProfilePictureForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['profile_picture']
+        widgets = {
+            'profile_picture': forms.FileInput(attrs={
+                'id': 'id_profile_picture',
+                'style': 'display: none;',
+            })
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfilePictureForm
 
 
+@login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == 'POST':
+        form = ProfilePictureForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfilePictureForm(instance=request.user)
+
+    return render(request, 'accounts/profile.html', {'form': form})
