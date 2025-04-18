27e899092f22201e4cbca629c56db2abe5a8d7c8
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-18 11:54:14 +0330

    Delete old pofile picture if user update picture

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ProfilePictureForm
@@ -5,12 +7,18 @@ from .forms import ProfilePictureForm
 
 @login_required
 def profile(request):
+    user = request.user
     if request.method == 'POST':
-        form = ProfilePictureForm(request.POST, request.FILES, instance=request.user)
+        old_picture_path = user.profile_picture.path if user.profile_picture else None
+        form = ProfilePictureForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
+            if 'profile_picture' in request.FILES:
+                # حذف عکس قبلی
+                if old_picture_path and os.path.exists(old_picture_path):
+                    os.remove(old_picture_path)
             form.save()
             return redirect('profile')
     else:
-        form = ProfilePictureForm(instance=request.user)
+        form = ProfilePictureForm(instance=user)
 
     return render(request, 'accounts/profile.html', {'form': form})
