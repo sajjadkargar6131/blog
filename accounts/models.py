af7381f40ec6232e330bab3eac73b208e3a3a87b
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-20 09:13:29 +0330

    Implement Activity model to track and record user actions and history

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .utils import get_unique_profile_path
+from config import settings
 
 
 class CustomUser(AbstractUser):
     profile_picture = models.ImageField(upload_to=get_unique_profile_path, null=True, blank=True, )
+
+
+class Activity(models.Model):
+    ACTION_CHOICES = [
+        ('login', 'ورود به حساب کاربری'),
+        ('profile_edit', 'ویرایش پروفایل'),
+        ('post_create', 'ارسال پست جدید'),
+        ('comment_create', 'ارسال دیدگاه'),
+    ]
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    action = models.CharField(max_length=50, choices=ACTION_CHOICES)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_action_display()}"
+
+    def get_icon(self):
+        icons = {
+            'login': 'fa-check-circle text-success',
+            'profile_edit': 'fa-edit text-primary',
+            'post_create': 'fa-file-alt text-info',
+            'comment_create': 'fa-comment text-warning',
+        }
+        return icons.get(self.action, 'fa-info-circle')
