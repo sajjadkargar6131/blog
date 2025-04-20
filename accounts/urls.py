d293852d6cffa963e457c7ecce179d6b46950778
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-20 09:10:43 +0330

    Implement a feature that allows users to change their password through the settings page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import CustomPasswordChangeView
 
 urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profile/picture/delete/', views.delete_profile_picture, name='delete_profile_picture'),
+    path("jj/change/", CustomPasswordChangeView.as_view(), name="account_change"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,22 @@
 import os
 
+from allauth.account.views import PasswordChangeView
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from allauth.account.forms import ChangePasswordForm
+from django.urls import reverse, reverse_lazy
+from django.http import JsonResponse, HttpResponseRedirect
+
 from .forms import ProfilePictureForm
 
 
 @login_required
 def profile(request):
     user = request.user
+    active_tab = request.GET.get("tab", "info")
+    change_password_form = ChangePasswordForm(request.user)
     if request.method == 'POST':
         old_picture_path = user.profile_picture.path if user.profile_picture else None
         form = ProfilePictureForm(request.POST, request.FILES, instance=user)
@@ -18,11 +26,16 @@ def profile(request):
                 if old_picture_path and os.path.exists(old_picture_path):
                     os.remove(old_picture_path)
             form.save()
+            messages.success(request, 'عکس پروفایل با موفقیت به روز شد.')
             return redirect('profile')
     else:
         form = ProfilePictureForm(instance=user)
 
-    return render(request, 'accounts/profile.html', {'form': form})
+    return render(request, 'accounts/profile.html', {
+        'form': form,
+        'change_password_form': change_password_form,
+        'active_tab': active_tab,
+    })
 
 
 @login_required
@@ -35,3 +48,29 @@ def delete_profile_picture(request):
     else:
         messages.info(request, 'عکسی برای حذف وجود نداشت.')
     return redirect('profile')
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = "accounts/profile.html"  # هم موفق، هم ناموفق همین قالب رو نشون بده
+
+    def get_default_success_url(self):
+        return reverse("profile") + "?tab=password"
+
+    def form_valid(self, form):
+        messages.success(self.request, "رمز عبور شما با موفقیت تغییر یافت.")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        if 'oldpassword' in form.errors:
+            messages.error(self.request, 'رمز عبور فعلی اشتباه است.')
+        else:
+            messages.error(self.request, 'لطفاً خطاهای فرم را بررسی کنید.')
+        context = self.get_context_data(form=form)
+        context['active_tab'] = 'password'
+        return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["change_password_form"] = context.get("form")  # این خط مهمه
+        return context
+
