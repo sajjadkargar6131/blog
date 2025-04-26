563c69853add450463f2bfe1e58a1cf0c2ab98fb
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-04-26 14:00:42 +0330

    show name of months in shmasi

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os.path
 from pathlib import Path
-
+import locale
+import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,6 +126,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+if sys.platform.startswith('win32'):
+    locale.setlocale(locale.LC_ALL, "Persian_Iran.UTF-8")
+else:
+    locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
