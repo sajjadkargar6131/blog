8cbb739c5fcd0e34547cb3e84dbadc0d7c0c57d4
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-05-13 11:36:13 +0330

    show users in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_superuser')
