a4318be2c50c73af08058507b77a1ad8d5f6dd8d
Sajjad <47275000+sajjadkargar6131@users.noreply.github.com>
2025-08-22 21:12:51 +0330

    fix bug

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,22 @@
-from django.db.models import Prefetch
-from django.shortcuts import get_object_or_404, redirect
-from django.shortcuts import render
+import os
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormMixin
 from django.views import generic
+from django.views.generic.edit import FormMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
 from django.contrib import messages
 from django.http import JsonResponse
 from django.utils.timezone import now
 from django.core.paginator import Paginator
+from django.db.models import Prefetch
 
 from accounts.models import Activity
-from .utils import get_clinet_ip, generate_unique_slug
-from .models import Post, Like, BookmarkPost, PostView, Category
+from .models import Post, Like, BookmarkPost, PostView, Category, Comment
 from .forms import PostCreateForm, CommentForm
+from .utils import get_clinet_ip, generate_unique_slug
 from shortener.models import ShortLink
 from taggit.models import Tag
-from .models import Comment
 
 
 class IndexListView(generic.ListView):
@@ -34,7 +33,6 @@ class IndexListView(generic.ListView):
         return context
 
 
-# --- Post Detail View ---
 class PostDetailView(generic.DetailView, FormMixin):
     model = Post
     template_name = 'blog/post_detail.html'
@@ -50,9 +48,7 @@ class PostDetailView(generic.DetailView, FormMixin):
         context = super().get_context_data(**kwargs)
         replies_prefetch = Prefetch('replies', queryset=Comment.objects.filter(publish=True).select_related('user'))
 
-        comments_qs = self.object.comments.filter(publish=True) \
-            .select_related('user') \
-            .prefetch_related(replies_prefetch)
+        comments_qs = self.object.comments.filter(publish=True).select_related('user').prefetch_related(replies_prefetch)
         context['comments'] = comments_qs.filter(parent__isnull=True).order_by('-datetime_created')
         context.update({
             'comments_count': self.object.comments_count,
@@ -67,11 +63,10 @@ class PostDetailView(generic.DetailView, FormMixin):
             context['liked'] = self.object.likes.filter(user=user).exists()
             context['bookmarked'] = self.object.bookmarks.filter(user=user).exists()
 
-        # ساخت لینک کوتاه
+        # لینک کوتاه
         full_url = self.request.build_absolute_uri(self.object.get_absolute_url())
-        short_link_obj, created = ShortLink.objects.get_or_create(original_url=full_url)
-        short_url = self.request.build_absolute_uri('/s/' + short_link_obj.short_code + '/')
-        context['short_url'] = short_url
+        short_link_obj, _ = ShortLink.objects.get_or_create(original_url=full_url)
+        context['short_url'] = self.request.build_absolute_uri('/s/' + short_link_obj.short_code + '/')
 
         return context
 
@@ -86,7 +81,6 @@ class PostDetailView(generic.DetailView, FormMixin):
             comment = form.save(commit=False)
             comment.post = self.object
             comment.user = request.user
-
             parent_id = form.cleaned_data.get('parent')
             if parent_id:
                 try:
@@ -96,8 +90,8 @@ class PostDetailView(generic.DetailView, FormMixin):
                     comment.parent = None
             else:
                 comment.parent = None
-
             comment.save()
+
             messages.success(request, 'نظر شما با موفقیت ثبت شد. در صورت تایید مدیر نمایش داده میشود')
             Activity.objects.create(
                 user=request.user,
@@ -105,13 +99,11 @@ class PostDetailView(generic.DetailView, FormMixin):
                 timestamp=now(),
                 description='ثبت یک کامنت جدید'
             )
-            return redirect(self.object.get_absolute_url())  # <=== اضافه کردن این خط
+            return redirect(self.object.get_absolute_url())
 
-
-        else:
-            context = self.get_context_data(object=self.object)
-            context['form'] = form
-            return self.render_to_response(context)
+        context = self.get_context_data(object=self.object)
+        context['form'] = form
+        return self.render_to_response(context)
 
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
@@ -125,7 +117,6 @@ class PostDetailView(generic.DetailView, FormMixin):
         return self.render_to_response(context)
 
 
-# --- Post Create View ---
 class PostCreateView(PermissionRequiredMixin, LoginRequiredMixin, generic.CreateView):
     form_class = PostCreateForm
     template_name = 'blog/post_create.html'
@@ -133,32 +124,23 @@ class PostCreateView(PermissionRequiredMixin, LoginRequiredMixin, generic.Create
 
     def form_valid(self, form):
         instance = form.save(commit=False)
-
-        # تنظیم نویسنده و slug
         instance.author = self.request.user
         instance.slug = generate_unique_slug(instance.title, instance=instance)
-
-        # ذخیره‌ی اولیه برای ایجاد id
         instance.save()
-
-        # بعد از اینکه instance ذخیره شد، حالا می‌تونیم m2m رو ست کنیم
-        form.instance = instance  # اطمینان حاصل کن که فرم هم از این instance استفاده کنه
+        form.instance = instance
         form.save_m2m()
 
-        # اگر new_category وارد شده بود
         new_category = form.cleaned_data.get('new_category')
         if new_category:
-            category, created = Category.objects.get_or_create(name=new_category)
+            category, _ = Category.objects.get_or_create(name=new_category)
             instance.categories.add(category)
 
-        # فعالیت
         Activity.objects.create(
             user=self.request.user,
             action='post_create',
             timestamp=now(),
             description='ارسال یک پست جدید'
         )
-
         messages.success(self.request, 'پست با موفقیت ثبت شد.')
         return redirect(instance.get_absolute_url())
 
@@ -168,14 +150,13 @@ class PostCreateView(PermissionRequiredMixin, LoginRequiredMixin, generic.Create
         return kwargs
 
 
-# --- Post Update View ---
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Post
     form_class = PostCreateForm
     template_name = 'blog/post_create.html'
 
     def get_object(self, queryset=None):
-        return Post.objects.get(slug=self.kwargs['slug'])
+        return get_object_or_404(Post, slug=self.kwargs['slug'])
 
     def get_initial(self):
         initial = super().get_initial()
@@ -188,42 +169,24 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
-
-        # تولید slug جدید
         self.object.slug = generate_unique_slug(self.object.title, instance=self.object)
-
-        # ذخیره اولیه (تا object دارای id شود)
         self.object.save()
+        form.instance = self.object
+        form.save_m2m()
 
-        # برچسب‌ها
-        tags = form.cleaned_data.get('tags')
-        if tags:
-            self.object.tags.set(tags)
-
-        # دسته‌بندی‌های انتخابی
-        categories = form.cleaned_data.get('categories')
-        if categories:
-            self.object.categories.set(categories)
-
-        # اگر کاربر دسته‌بندی جدیدی وارد کرده
         new_category = form.cleaned_data.get('new_category')
         if new_category:
-            category, created = Category.objects.get_or_create(name=new_category)
+            category, _ = Category.objects.get_or_create(name=new_category)
             self.object.categories.add(category)
 
-        # پیام موفقیت
         messages.success(self.request, 'پست با موفقیت به‌روزرسانی شد.')
-
-        # ثبت اکشن در جدول فعالیت‌ها
         Activity.objects.create(
             user=self.request.user,
             action='post_edit',
             timestamp=now(),
             description='ویرایش پست'
         )
-
-        # جلوگیری از ذخیره مجدد فرم
-        return redirect(self.get_success_url())
+        return redirect(self.object.get_absolute_url())
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -231,14 +194,13 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView
         return kwargs
 
 
-# --- Post Delete View ---
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Post
     template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('blog_index')
 
     def get_object(self, queryset=None):
-        return Post.objects.get(slug=self.kwargs['slug'])
+        return get_object_or_404(Post, slug=self.kwargs['slug'])
 
     def test_func(self):
         obj = self.get_object()
@@ -248,10 +210,10 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView
         obj = self.get_object()
         if obj.cover:
             try:
-                obj.cover.delete(save=False)
-            except Exception as e:
-                print(f"Error deleting file: {e}")
-
+                if os.path.isfile(obj.cover.path):
+                    obj.cover.delete(save=False)
+            except (FileNotFoundError, ValueError):
+                pass
         messages.success(self.request, "پست با موفقیت حذف شد")
         Activity.objects.create(
             user=self.request.user,
@@ -262,7 +224,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView
         return super().form_valid(form)
 
 
-# --- Like Post ---
 @login_required
 def like_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -274,11 +235,9 @@ def like_post(request, post_id):
         else:
             Like.objects.create(post=post, user=user)
             liked = True
-        like_count = post.likes.count()
-        return JsonResponse({"liked": liked, "likes_count": like_count})
+        return JsonResponse({"liked": liked, "likes_count": post.likes.count()})
 
 
-# --- Bookmark Post ---
 @login_required
 def bookmark_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -290,26 +249,16 @@ def bookmark_post(request, post_id):
         else:
             BookmarkPost.objects.create(post=post, user=user)
             bookmarked = True
-        bookmarked_count = post.bookmarks.count()
-        return JsonResponse({"bookmarked": bookmarked, "bookmarks_count": bookmarked_count})
+        return JsonResponse({"bookmarked": bookmarked, "bookmarks_count": post.bookmarks.count()})
 
 
 MONTH_NAMES_FA = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن',
                   'اسفند']
 
 
-# --- Archive Month ---
 def archive_month(request, year, month):
-    # ساخت رشته‌ای شبیه "1404-02"
     month_str = f"{year}-{int(month):02d}"
-
-    # فیلتر پست‌هایی که shamsi_date با این ماه شروع میشه
-    posts = Post.objects.filter(
-        shamsi_date__startswith=month_str,
-        status='pub'
-    ).select_related('author')
-
-    # ساخت نام فارسی ماه برای عنوان صفحه
+    posts = Post.objects.filter(shamsi_date__startswith=month_str, status='pub').select_related('author')
     month_index = int(month)
     month_name = f"{MONTH_NAMES_FA[month_index - 1]} {year}"
 
@@ -323,7 +272,6 @@ def archive_month(request, year, month):
     })
 
 
-# --- Category Post List View ---
 class CategoryPostListView(generic.ListView):
     model = Post
     template_name = 'blog/posts_by_category.html'
@@ -341,7 +289,6 @@ class CategoryPostListView(generic.ListView):
         return context
 
 
-# --- Post List by Tag ---
 class PostListByTagView(generic.ListView):
     model = Post
     context_object_name = 'list'
@@ -351,7 +298,7 @@ class PostListByTagView(generic.ListView):
     def get_queryset(self):
         tag_slug = self.kwargs['tag_slug']
         tag = get_object_or_404(Tag, slug=tag_slug)
-        return Post.objects.filter(tags=tag, status='pub').select_related('author')  # optional: برای بهینه‌سازی
+        return Post.objects.filter(tags=tag, status='pub').select_related('author')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
